2768c59532f6797627811d6f94a5e3ad8a4a8995
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-06-03 21:28:55 +0530

    fix

--- modules/people/manifests/sahilm.pp
+++ modules/people/manifests/sahilm.pp
@@ -1,11 +1,3 @@
 class people::sahilm {
   $home = "/Users/${::luser}"
-
-  sudoers { 'NAME':
-    users => "${::luser}",
-    commands => 'ALL=(ALL) NOPASSWD: ALL',
-    hosts => 'ALL',
-    ensure => present,
-    type => 'user_spec'
-  }
 }
